387764d12f99d150a3fd70577010cc1069a2fd35
Ilya Matveyev <Ilia2001OK@gmail.com>
2025-08-31 16:40:04 +0300

    Добавил DTO для модели Task: TaskCreateRequest; TaskUpdateRequest; TaskResponse;

---
+++ src/TaskTracker.Application/DTOs/TaskCreateRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskTracker.Application.DTOs
+{
+	public record TaskCreateRequest(
+		string Title, 
+		string Description,
+		bool IsCompleted,
+		Guid ProjectId);
+}
---
+++ src/TaskTracker.Application/DTOs/TaskUpdateRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskTracker.Application.DTOs
+{
+	public record TaskUpdateRequest(
+		string Title,
+		string Description,
+		bool IsCompleted,
+		Guid ProjectId);
+}
