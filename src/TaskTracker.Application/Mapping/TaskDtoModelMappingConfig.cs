e9ac4336a42843b47e04d0a04cff449acccf91ea
Ilya Matveyev <Ilia2001OK@gmail.com>
2025-08-31 17:18:36 +0300

    Добавил конфигурации маппинга Task.

---
+++ src/TaskTracker.Application/Mapping/TaskDtoModelMappingConfig.cs
@@ -0,0 +1,46 @@
+﻿using Mapster;
+using TaskTracker.Application.DTOs;
+using TaskTracker.Domain.Models;
+
+namespace TaskTracker.Application.Mapping
+{
+	public class TaskDtoModelMappingConfig : IRegister
+	{
+		public void Register(TypeAdapterConfig config)
+		{
+			// TaskCreateRequest -> Task
+			TypeAdapterConfig<TaskCreateRequest, Task>
+				.NewConfig()
+				.Map(dest => dest.Id, _ => Guid.NewGuid())
+				.Map(dest => dest.Title, src => src.Title)
+				.Map(dest => dest.Description, src => src.Description)
+				.Map(dest => dest.IsCompleted, src => src.IsCompleted)
+				.Map(dest => dest.CreatedAt, _ => DateTime.UtcNow)
+				.Map(dest => dest.UpdatedAt, _ => DateTime.UtcNow)
+				.Map(dest => dest.ProjectId, src => src.ProjectId)
+				.MaxDepth(2);
+
+			// TaskUpdateRequest -> Task
+			TypeAdapterConfig<TaskUpdateRequest, Task>
+				.NewConfig()
+				.Map(dest => dest.Title, src => src.Title)
+				.Map(dest => dest.Description, src => src.Description)
+				.Map(dest => dest.IsCompleted, src => src.IsCompleted)
+				.Map(dest => dest.UpdatedAt, _ => DateTime.UtcNow)
+				.Map(dest => dest.ProjectId, src => src.ProjectId)
+				.MaxDepth(2);
+
+			// Task -> TaskResponse
+			TypeAdapterConfig<Task, TaskResponse>
+				.NewConfig()
+				.Map(dest => dest.Id, src => src.Id)
+				.Map(dest => dest.Title, src => src.Title)
+				.Map(dest => dest.Description, src => src.Description)
+				.Map(dest => dest.IsCompleted, src => src.IsCompleted)
+				.Map(dest => dest.CreatedAt, src => src.CreatedAt)
+				.Map(dest => dest.UpdatedAt, src => src.UpdatedAt)
+				.Map(dest => dest.ProjectId, src => src.ProjectId)
+				.MaxDepth(2);
+		}
+	}
+}
